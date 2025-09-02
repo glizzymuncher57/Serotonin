d300fc67e41c0b15f9e86de90dde3c7eaf50c3b2
Daniel <dsfxzfu@gmail.com>
2025-09-02 02:44:06 +0100

    Implement UI API Wrapper with Element and Container objects

---
+++ Utility/UIAPIWrapper.lua
@@ -0,0 +1,127 @@
+local Library = {}
+
+
+-- ============= Element Object =============
+local Element = {}
+Element.__index = Element
+
+
+function Element:Get()
+	return ui.getValue(self.TabRef, self.ContainerRef, self.Name)
+end
+
+
+function Element:Set(value)
+	ui.setValue(self.TabRef, self.ContainerRef, self.Name, value)
+end
+
+
+function Element:Visible(state)
+	ui.setVisibility(self.TabRef, self.ContainerRef, self.Name, state)
+end
+
+
+-- ============= Container Object =============
+local Container = {}
+Container.__index = Container
+
+
+local function MakeElement(tabRef, containerRef, name)
+	return setmetatable({
+		TabRef = tabRef,
+		ContainerRef = containerRef,
+		Name = name,
+	}, Element)
+end
+
+
+function Container:Checkbox(name, inLine)
+	ui.newCheckbox(self.TabRef, self.Ref, name, inLine)
+	return MakeElement(self.TabRef, self.Ref, name)
+end
+
+
+function Container:SliderInt(name, min, max, default)
+	ui.newSliderInt(self.TabRef, self.Ref, name, min, max, default)
+	return MakeElement(self.TabRef, self.Ref, name)
+end
+
+
+function Container:SliderFloat(name, min, max, default)
+	ui.newSliderFloat(self.TabRef, self.Ref, name, min, max, default)
+	return MakeElement(self.TabRef, self.Ref, name)
+end
+
+
+function Container:Dropdown(name, options, defaultIndex)
+	ui.newDropdown(self.TabRef, self.Ref, name, options, defaultIndex)
+	local element = MakeElement(self.TabRef, self.Ref, name)
+	element.Get = function()
+		local index = ui.getValue(self.TabRef, self.Ref, name)
+		return options[index + 1]
+	end
+	return element
+end
+
+
+function Container:Multiselect(name, options)
+	ui.newMultiselect(self.TabRef, self.Ref, name, options)
+	return MakeElement(self.TabRef, self.Ref, name)
+end
+
+
+function Container:Colorpicker(name, defaultColor, inLine)
+	ui.newColorpicker(self.TabRef, self.Ref, name, defaultColor, inLine)
+	return MakeElement(self.TabRef, self.Ref, name)
+end
+
+
+function Container:InputText(name, defaultText)
+	ui.newInputText(self.TabRef, self.Ref, name, defaultText)
+	return MakeElement(self.TabRef, self.Ref, name)
+end
+
+
+function Container:Button(name, callback)
+	ui.newButton(self.TabRef, self.Ref, name, callback)
+	local element = MakeElement(self.TabRef, self.Ref, name)
+	element.Get = nil
+	element.Set = nil
+	return element
+end
+
+
+function Container:Listbox(name, options, callback)
+	ui.newListbox(self.TabRef, self.Ref, name, options, function()
+		if callback then
+			local index = ui.getValue(self.TabRef, self.Ref, name)
+			local selected = options[index + 1]
+			callback(selected)
+		end
+	end)
+	return MakeElement(self.TabRef, self.Ref, name)
+end
+
+
+-- ============= Tab Object =============
+local Tab = {}
+Tab.__index = Tab
+
+
+function Tab:Container(containerRef, displayName, options)
+	ui.newContainer(self.Ref, containerRef, displayName, options or {})
+	return setmetatable({
+		TabRef = self.Ref,
+		Ref = containerRef,
+	}, Container)
+end
+
+
+-- ============= Root =============
+function Library.NewTab(tabRef, displayName)
+	ui.newTab(tabRef, displayName)
+	return setmetatable({ Ref = tabRef }, Tab)
+end
+
+
+return Library
